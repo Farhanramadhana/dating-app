567a27b498eeb0dc5bc3b4e753501655b2dcf643
Farhan Ramadhana <farhanramadhana97@gmail.com>
2024-01-19 16:59:08 +0700

    integrate redis

---
+++ app/redis.go
@@ -0,0 +1,41 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type redisClient struct {
+	client *redis.Client
+}
+
+type Redis interface {
+	RedisSet(ctx context.Context, key string, value any) error
+	RedisGet(ctx context.Context, key string) string
+}
+
+func NewRedisClient(host, port, password string) Redis {
+	client := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", host, port),
+		Password: password, // no password set
+		DB:       0,        // use default DB
+	})
+
+	res := client.Ping(context.Background())
+	log.Println("Redis: ", res)
+	return &redisClient{client}
+}
+
+func (r *redisClient) RedisSet(ctx context.Context, key string, value any) error {
+	v := r.client.Set(ctx, key, value, 0)
+
+	return v.Err()
+}
+
+func (r *redisClient) RedisGet(ctx context.Context, key string) string {
+	v := r.client.Get(ctx, key)
+	return v.Val()
+}
--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	authHandler "dating-app/src/auth/delivery"
 	authRepository "dating-app/src/auth/repository"
 	authUsecase "dating-app/src/auth/usecase"
+	swipeHandler "dating-app/src/swipe/delivery"
+	swipeRepository "dating-app/src/swipe/repository"
+	swipeUsecase "dating-app/src/swipe/usecase"
 	userHandler "dating-app/src/user/delivery"
 	userRepository "dating-app/src/user/repository"
 	userUsecase "dating-app/src/user/usecase"
@@ -26,6 +29,8 @@ func main() {
 	}
 
 	db, err := app.InitDatabase()
+	redis := app.NewRedisClient(os.Getenv("redis_host"), os.Getenv("redis_port"), os.Getenv("redis_password"))
+
 	if err != nil {
 		errMessage := fmt.Sprintf("error running database: %s ", err)
 		panic(errMessage)
@@ -35,26 +40,27 @@ func main() {
 	// repository
 	authRepository := authRepository.NewAuthRepository(db)
 	userRepository := userRepository.NewUserRepository(db)
+	swipeRepository := swipeRepository.NewSwipeRepository(db, redis)
 
 	// usecase
 	authUsecase := authUsecase.NewAuthUsecase(authRepository)
 	userUsecase := userUsecase.NewUserUsecase(userRepository)
+	swipeUsecase := swipeUsecase.NewSwipeUsecase(swipeRepository)
 
 	// handler
 	authHandler.NewAuthHandler(r, authUsecase)
+	userHandler.NewUserHandler(r, userUsecase)
+	swipeHandler.NewSwipeHandler(r, swipeUsecase)
 
-	// Example protected route
+	// helath check
 	r.Handle("/health", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		utils.RespondWithJSON(w, http.StatusOK, map[string]string{"message": "hello world"})
 	})).Methods("GET")
 
-	userHandler.NewUserHandler(r, userUsecase)
-	
 	fmt.Printf("starting the server %s on port %s \n", time.Now().Format(constant.YYYY_MM_DD_HH_MM_SS), os.Getenv("port"))
-	
+
 	http.Handle("/", r)
-	addr := fmt.Sprintf(":%s", os.Getenv("port"))
-	err = http.ListenAndServe(addr, nil)
+	err = http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("port")), nil)
 	if err != nil {
 		errMessage := fmt.Sprintf("server error %s ", err)
 		panic(errMessage)
