f4a052751d9177f57756f92505741c6924ad6327
Farhan Ramadhana <farhanramadhana97@gmail.com>
2024-01-19 21:29:50 +0700

    update constant

--- model/constant/constant.go
+++ model/constant/constant.go
@@ -2,3 +2,6 @@ package constant
 
 const YYYY_MM_DD_HH_MM_SS = "2006-01-02 15:04:05"
 const YYYY_MM_DD = "2006-01-02"
+const USER_SWIPE_KEY = "user_swipe_%v"
+const MAX_SWIPE = 10
+const USER_PROFILE_APPEARED = "user_profile_appeared_%v"
